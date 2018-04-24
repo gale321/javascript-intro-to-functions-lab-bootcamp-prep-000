acd245a52578756630615e4231f5681f8cca62de
Ryan Ponthier <ryanjponthier@gmail.com>
2018-04-24 16:00:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   return string.toLowerCase()
-if sayHiToGrandma === sayHiToGrandma.toUpperCase()
+if sayHiToGrandma(string) === sayHiToGrandma(string.toUpperCase)
 
 }
